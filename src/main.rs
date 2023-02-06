b90658628205950e9c2cd054a4f53ab49152481e
Leah <105241362+Newt93@users.noreply.github.com>
2023-02-05 23:34:16 -0600

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("What is your name?: ");
+    let mut your_name = String::new();
+    io::stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    
+    println!("Hello {your_name}");
+
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Please input your guess: ");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    println!("You guessed: {guess}");
+    println!("The secret number is: {secret_number}");
+
+
+}
